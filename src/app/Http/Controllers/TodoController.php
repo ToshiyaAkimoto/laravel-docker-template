fbebd7f4efb5655e21eb3463510eb0208489379d
ToshiyaAkimoto <banno1ta@gmail.com>
2024-06-11 15:22:20 +0900

    add:resources/views/todo/create.blade.php を作成 feat:index.blade.phpに、Todoを追加ボタンを追加 feat:TodoController.phpに、create関数を追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -16,4 +16,11 @@ class TodoController extends Controller
         return view('todo.index', ['todoList' => $todoList]);//view('todo.index') は resources/views/ に配置されている index.blade.php のことを指している
     //view関数の第二引数の連想配列は、[blade内での変数名 => 代入したい値]を意味します。
     }
+
+    public function create()
+    {
+    // TODO: 第1引数を指定
+    return view('todo.create'); // 追記
+    }
+
 }
--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -28,7 +28,7 @@
         <div class="row justify-content-center">
           <div class="col-md-8">
             <p class="text-left">
-              <a class="btn btn-success" href="">ToDoを追加</a>
+              <a class="btn btn-success" href="http://localhost:8080/todo/create">ToDoを追加</a>
             </p>
             <div class="card">
               <div class="card-header">
@@ -37,7 +37,7 @@
               <div class="list-group list-group-flush">
                 @foreach ($todoList as $todo)
                   <div class="d-flex align-items-center p-2">
-                    <span class="col-9">{{ $todo->content }}</span><!--{{ }}で囲うことで、PHPの処理として認識される-->
+                    <span class="col-9">{{ $todo->content }}</span>
                   </div>
                 @endforeach <!--「@」はディレクティブ 一般的なPHPの制御構文の便利な短縮記述方法を提供してくれる-->
               </div>
