3c35a0d868786d30002130bafa187a5893d4adc7
ToshiyaAkimoto <banno1ta@gmail.com>
2024-06-11 15:00:20 +0900

    feat:index.blade.phpに、Todoを追加ボタンと一覧表示機能を追加

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -10,8 +10,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
-<body>
-  <div id="app">
+<div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
         <a class="navbar-brand" href="/todo">ToDo</a>
@@ -28,11 +27,19 @@
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-8">
+            <p class="text-left">
+              <a class="btn btn-success" href="">ToDoを追加</a>
+            </p>
             <div class="card">
               <div class="card-header">
                 ToDo一覧
               </div>
               <div class="list-group list-group-flush">
+                @foreach ($todoList as $todo)
+                  <div class="d-flex align-items-center p-2">
+                    <span class="col-9">{{ $todo->content }}</span><!--{{ }}で囲うことで、PHPの処理として認識される-->
+                  </div>
+                @endforeach <!--「@」はディレクティブ 一般的なPHPの制御構文の便利な短縮記述方法を提供してくれる-->
               </div>
             </div>
           </div>
