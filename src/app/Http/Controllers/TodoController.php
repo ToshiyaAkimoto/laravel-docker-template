84792ac0835221363058ec4f3e3f4f937fcbfa3d
ToshiyaAkimoto <banno1ta@gmail.com>
2024-06-11 14:44:20 +0900

    add:app/Todoを追加　feat:TodoControllerにTodoと連携するためのコードを追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Todo;//Appフォルダ内のTodoファイルを使用する
+
+use Illuminate\Http\Request;//
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();//クラスを実行(TodoはApp\Todoにある) todosテーブルを使用する
+        $todoList = $todo->all();//DBのtodosテーブル内のデータを取得
+
         return view('todo.index');//view('todo.index') は resources/views/ に配置されている index.blade.php のことを指している
     }
 }
