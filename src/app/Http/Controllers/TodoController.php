7eb61c48800fa15c046ac1760dae110fb94ef4d9
ToshiyaAkimoto <banno1ta@gmail.com>
2024-06-13 12:28:49 +0900

    feat:新規作成機能に一括代入機能を実装 TodoController.phpとTodo.phpに一括代入機能のためのコードを追記

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -24,12 +24,14 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all(); // 変更
+        dd($inputs); // 追記
 
         // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
         $todo = new Todo();
+        $todo->user_id = Auth::id();//攻撃者のユーザIDを保存
         // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->content = $content;
+        $todo->fill($inputs); // fill()によって、$todo->連想配列のキー=連想配列のバリューを自動的に行ってくれる ただし、fillableで許可しているものだけ
         // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
         $todo->save();
         return redirect()->route('todo.index');
--- src/app/Todo.php
+++ src/app/Todo.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     protected $table = 'todos';//MySQLとの連携用 テーブルはtodosテーブルを使用する
+
+    protected $fillable = [//->fillによってModelに代入可能なプロパティを記載する　これ以外の値は代入されない
+        'content',
+    ];
 }
