738fe60a5f5aaa5b9b9a6fabed881a91d49f06a5
ToshiyaAkimoto <banno1ta@gmail.com>
2024-06-20 19:53:27 +0900

    add: resources/views/todo/show.blade.phpを追加 feat:index.blade.phpに詳細ボタンを追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -10,7 +10,7 @@ class TodoController extends Controller
     public function index()
     {
         $todo = new Todo();//クラスを実行(TodoはApp\Todoにある) todosテーブルを使用する
-        $todoList = $todo->all();//DBのtodosテーブル内のデータを取得
+        $todoList = $todo->all();//DBのtodosテーブル内のデータを取得 all()によって、todoで指定したテーブルの全データを取得している
 
         return view('todo.index', ['todoList' => $todoList]);//view('todo.index') は resources/views/ に配置されている index.blade.php のことを指している
     //view関数の第二引数の連想配列は、[blade内での変数名 => 代入したい値]を意味します。
@@ -19,22 +19,29 @@ class TodoController extends Controller
     public function create()
     {
     // TODO: 第1引数を指定
-    return view('todo.create'); // 追記
+    return view('todo.create'); // このページを表示する
     }
 
     public function store(Request $request)
     {
+//C:\Users\banga\Desktop\GizTech作業\Laravel\src\vendor\laravel\framework\src\Illuminate\Http内にある、Request.phpを参照する
+
         $inputs = $request->all(); // 変更
-        dd($inputs); // 追記
+        //dd($inputs); // 追記
 
         // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
         $todo = new Todo();
-        $todo->user_id = Auth::id();//攻撃者のユーザIDを保存
+        //$todo->user_id = Auth::id();//攻撃者(入力者)のユーザIDを保存
         // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
         $todo->fill($inputs); // fill()によって、$todo->連想配列のキー=連想配列のバリューを自動的に行ってくれる ただし、fillableで許可しているものだけ
         // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
         $todo->save();
         return redirect()->route('todo.index');
     }
-
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);//idが一致するデータを取得している
+        return view('todo.show', ['todo' => $todo]); // 追記
+    }
 }
--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,11 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+//C:\Users\banga\Desktop\GizTech作業\Laravel\src\vendor\laravel\framework\src\Illuminate\Database\Eloquent　にあるデータ
 
 class Todo extends Model
 {
     protected $table = 'todos';//MySQLとの連携用 テーブルはtodosテーブルを使用する
-
     protected $fillable = [//->fillによってModelに代入可能なプロパティを記載する　これ以外の値は代入されない
         'content',
     ];
