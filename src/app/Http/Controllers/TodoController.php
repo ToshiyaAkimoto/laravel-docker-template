c4c7545684cc28e1776e4407b40f95cd166dc7bf
ToshiyaAkimoto <banno1ta@gmail.com>
2024-06-20 20:05:22 +0900

    refactor: new Todo()をリファクタリングし、保守性を向上

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    private $todo; // 追記
+
     public function index()
     {
-        $todo = new Todo();//クラスを実行(TodoはApp\Todoにある) todosテーブルを使用する
-        $todoList = $todo->all();//DBのtodosテーブル内のデータを取得 all()によって、todoで指定したテーブルの全データを取得している
+        $todoList = $this->todo->all();//DBのtodosテーブル内のデータを取得 all()によって、todoで指定したテーブルの全データを取得している
 
         return view('todo.index', ['todoList' => $todoList]);//view('todo.index') は resources/views/ に配置されている index.blade.php のことを指している
     //view関数の第二引数の連想配列は、[blade内での変数名 => 代入したい値]を意味します。
@@ -30,18 +31,20 @@ class TodoController extends Controller
         //dd($inputs); // 追記
 
         // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo = new Todo();
         //$todo->user_id = Auth::id();//攻撃者(入力者)のユーザIDを保存
         // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->fill($inputs); // fill()によって、$todo->連想配列のキー=連想配列のバリューを自動的に行ってくれる ただし、fillableで許可しているものだけ
+        $this->todo->fill($inputs); // 変更 // fill()によって、$todo->連想配列のキー=連想配列のバリューを自動的に行ってくれる ただし、fillableで許可しているものだけ
         // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-        $todo->save();
+        $this->todo->save(); // 変更
         return redirect()->route('todo.index');
     }
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);//idが一致するデータを取得している
+        $todo = $this->todo->find($id);//idが一致するデータを取得している
         return view('todo.show', ['todo' => $todo]); // 追記
+    }
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo; // 追記
     }
 }
